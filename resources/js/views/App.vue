ac6a94aae6131b3ab9339dada6a0d880ff2ecae9
DannySilv <danielesilvestre@outlook.it>
2022-07-06 01:14:59 +0200

    Completed

---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Front Office | Home</h1>
+    <ExampleComponent />
+  </div>
+</template>
+
+<script>
+import ExampleComponent from '../components/ExampleComponent.vue';
+export default {
+  name: 'App',
+  components: {
+    ExampleComponent
+  }
+}
+</script>
+
+<style>
+</style> 
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.dashboard')
+
+@section('main')
+
+<h1>Welcome back {{ $user->name }}, this is your home!</h1>
+
+@endsection
