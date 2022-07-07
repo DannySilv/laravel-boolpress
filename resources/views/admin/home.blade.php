8f6970c2f1ebd877b6a633833518577690ed13a2
DannySilv <danielesilvestre@outlook.it>
2022-07-07 21:23:04 +0200

    Completed Bonus

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,5 +3,11 @@
 @section('content')
 
 <h1>Welcome back {{ $user->name }}, this is your home!</h1>
+@if ($userInfo)
+    
+  <p>Phone: {{ $userInfo->phone }}</p>
+  <p>Address: {{ $userInfo->address }}</p>
+
+@endif
 
 @endsection
